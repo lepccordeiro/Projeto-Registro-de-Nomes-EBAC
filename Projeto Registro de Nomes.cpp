fb931f765c9c215d590883c7370e98fffd05d037
lepccordeiro <luiscordeiro@yahoo.com>
2024-09-27 17:07:04 -0300

    Primeira versão do Projeto_Modulo 2
    Primeira versão para aprendizagem do C++

---
+++ Projeto Registro de Nomes.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+	setlocale(LC_ALL, "portuguese");
+	printf("### Cartório EBAC ###\n\n");
+	printf("Escolha a Opção desejada:\n\n\n");
+	printf("\t1 - Registrar Nome\n");
+	printf("\t2 - Consultar Nome\n");
+	printf("\t3 - Apagar Nome do Registro\n\n\n\n");
+	
+	printf("Esse Software é de livre uso dos alunos da EBAC como parte do curso \"Profissão TI: do Zero ao Pro\"\n\n");
+
+}
