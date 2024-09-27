1a1b688fa2e64d2daeba4be67279fd06a6ddb2e9
lepccordeiro <luiscordeiro@yahoo.com>
2024-09-27 17:08:29 -0300

    2 versão
    adicionadas funcionalidades que permitem escolher as opções

--- Projeto Registro de Nomes.cpp
+++ Projeto Registro de Nomes.cpp
@@ -1,16 +1,54 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <locale.h>
+#include <stdio.h> // biblioteca de comunicação com o usuário
+#include <stdlib.h> // biblioteca de alocação de espaço de memoria
+#include <locale.h> // biblioteca de alocação de texto por região
 
 int main()
 {
-	setlocale(LC_ALL, "portuguese");
-	printf("### Cartório EBAC ###\n\n");
-	printf("Escolha a Opção desejada:\n\n\n");
-	printf("\t1 - Registrar Nome\n");
-	printf("\t2 - Consultar Nome\n");
-	printf("\t3 - Apagar Nome do Registro\n\n\n\n");
+	int opcao=0; // definindo variaveis
+	int x=1;
 	
-	printf("Esse Software é de livre uso dos alunos da EBAC como parte do curso \"Profissão TI: do Zero ao Pro\"\n\n");
-
+	for(x=1;x=1;)
+	
+	{
+		
+		setlocale(LC_ALL, "portuguese"); // definindo a linguagem
+		
+		system("cls");	
+		
+		printf("### Cartório EBAC ###\n\n"); // inicio do menu
+		printf("Escolha a Opção desejada:\n\n\n");
+		printf("\t1 - Registrar Nome\n");
+		printf("\t2 - Consultar Nome\n");
+		printf("\t3 - Apagar Nome do Registro\n\n\n\n");
+		printf("Opção: "); // fim do menu
+	
+		scanf("%d", &opcao); // armazenando a opção do usuario
+	
+		system("cls");
+		
+		switch (opcao)
+		{
+			case 1:
+			printf("Voce escolheu registrar nomes!\n");
+			system("pause");
+			break;
+			
+			case 2:
+			printf("Voce escolheu consultar nomes!\n");
+			system("pause");
+			break;
+			
+			case 3:
+			printf("Voce escolheu apagar nomes!\n");
+			system("pause");
+			break;
+			
+			default:
+			printf("Infelizmente esta opção não está disponível! Favor escolher entre as opções 1, 2 ou 3.\n");
+			system("pause");
+			break;	
+	
+			
+		}
+	}
 }
