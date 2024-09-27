4384da86e4adffd580ead750be7aa718bdada0c0
lepccordeiro <luiscordeiro@yahoo.com>
2024-09-27 17:12:38 -0300

    versão inserindo dados
    nessa versão é possível inserir os dados no sistema

--- Projeto Registro de Nomes.cpp
+++ Projeto Registro de Nomes.cpp
@@ -1,18 +1,96 @@
 #include <stdio.h> // biblioteca de comunicação com o usuário
 #include <stdlib.h> // biblioteca de alocação de espaço de memoria
 #include <locale.h> // biblioteca de alocação de texto por região
+#include <string.h> // biblioteca responsavel pelas string
 
 
 int registrar()
 {
-	printf("Voce escolheu registrar nomes!\n");
-	system("pause");	
+	setlocale(LC_ALL, "portuguese"); // definindo a linguagem
+	
+	char arquivo[40]; //criação da variavel "arquivo" com o tamanho de 40 caracteres (comando char)
+	char cpf[40];
+	char nome[40];
+	char sobrenome[40];
+	char cargo[40];
+	
+	printf("Digite o CPF a ser cadastrado: "); // pergunta que aparece ao usuário
+	scanf("%s",cpf); // será salvo como na vaiavel (comando "%") como uma string (letra "s"), neste caso "cpf" que é um conjunto de variáveis
+	
+	strcpy(arquivo,cpf); // responsavel por copiar os valores da string	
+	
+	FILE *file; // cria o arquivo "file" na pasta
+	file = fopen(arquivo,"w"); // cria o arquivo na pasta, "w" (write) permite escrever o conteudo do arquivo, fopen informa que é para abrir o arquivo
+	fprintf(file,cpf); //salva dentro do arquivo "file" a variavel "cpf" (comando fprintf)
+	fclose(file); // fecha a inserção no arquivo "file"
+	
+	file = fopen(arquivo,"a");  // abrir e atualizar um arquivo que ja esta salvo na pasta file, (comando "a" = atualizar)
+	fprintf(file,","); // cria um espaço entre os dados, apra que não fique tudo junto na mesma linha
+	fclose(file); 
+	
+	printf("Digite o nome a ser cadastrado: ");
+	scanf("%s",nome);
+	
+	file = fopen(arquivo,"a");
+	fprintf(file,nome);
+	fclose(file);
+		
+	file = fopen(arquivo,"a");  
+	fprintf(file,","); 
+	fclose(file); 
+	
+	printf("Digite o sobrenome a ser cadastrado: ");
+	scanf("%s",sobrenome);
+	
+	file = fopen(arquivo,"a");
+	fprintf(file,sobrenome);
+	fclose(file);
+	
+	file = fopen (arquivo,"a");
+	fprintf(file,",");
+	fclose(file);
+	
+	printf("Digite o cargo a ser cadastrado: ");
+	scanf("%s",cargo);
+	
+	file = fopen(arquivo,"a");
+	fprintf(file,cargo);
+	fclose(file);
+	
+	file = fopen(arquivo,"a");
+	fprintf(file,",");
+	fclose(file);
+	
+	system("pause");
+	
+	
 }
 
 int consultar()
 {
-	printf("Voce escolheu consultar nomes!\n");
-	system("pause");	
+	setlocale(LC_ALL, "portuguese"); // definindo a linguagem
+	
+	char cpf[40]; // cria novamente string cpf nesta função
+	char conteudo[200];	// cria uma variavel que vai buscar o que ja esta salvo, o programa vai ler os arquivos
+	
+	printf("Digite o cpf a ser consultado: ");
+	scanf("%s",cpf);
+	
+	FILE *file;
+	file = fopen(cpf,"r"); // abre o arquivo "cpf" e lê o conteudo ("r" de read)
+	
+	if (file==NULL) // se o valor não existir, então...
+	{
+		printf("Esse cpf não está cadastrado.\n"); // informe ao usuário
+	}
+	
+	while(fgets(conteudo, 200, file) !=NULL) // enquanto houver letras dentro do arquivo "file", continue trazendo ate o limite de 200 caracteres até encontrar valo nulo
+	{
+		printf("\n Essas são as informações do cpf solicitado.");
+		printf("%s", conteudo);
+		printf("\n\n");
+	}
+	system("pause");
 }
 
 int deletar()
